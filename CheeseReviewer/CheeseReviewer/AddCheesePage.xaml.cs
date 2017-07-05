258995a2efc28ce5a89255ea5af79a70d97a3f12
Matt <matthew.lee@ecs.vuw.ac.nz>
2017-07-06 10:58:09 +1200

    Added some validations to the inputs

--- CheeseReviewer/CheeseReviewer/AddCheesePage.xaml.cs
+++ CheeseReviewer/CheeseReviewer/AddCheesePage.xaml.cs
@@ -12,10 +12,20 @@ namespace CheeseReviewer
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddCheesePage : ContentPage
 	{
+
+        string cheeseBrand;
+        string cheeseType;
+        string cheeseComments;
+        string cheeseLocation;
+        double cheesePrice;
+        int cheeseRating;
+
 		public AddCheesePage ()
 		{
 			InitializeComponent ();
+            price.SetBinding(Entry.TextProperty, new Binding("Price", converter: new DecimalConverter()));
         }
+
 
         void EditorTextChanged(object sender, TextChangedEventArgs e)
         {
@@ -26,11 +36,40 @@ namespace CheeseReviewer
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
-
+            double stepValue = 1.0;
+            var newStep = Math.Round(args.NewValue / stepValue);
+            double newValue = newStep * stepValue;
+            sliderValue.Text = newValue.ToString();
+            cheeseRating = int.Parse(sliderValue.Text);
         }
 
         void OnAdd(object sender, ValueChangedEventArgs args)
         {
+            if (String.IsNullOrEmpty(brand.Text))
+            {
+                DisplayAlert("Cheese Brand Empty", "You need to fill out the cheese's brand to proceed", "OK");
+                return;
+            }else if (String.IsNullOrEmpty(type.Text))
+            {
+                DisplayAlert("Cheese Type Empty", "You need to fill out the cheese's type to proceed", "OK");
+                return;
+            }
+            else if (String.IsNullOrEmpty(location.Text))
+            {
+                DisplayAlert("Cheese Purchase Location Empty", "You need to fill out the cheese's purchase location to proceed", "OK");
+                return;
+            }
+            else if (String.IsNullOrEmpty(price.Text))
+            {
+                DisplayAlert("Cheese Price Empty", "You need to fill out the cheese's price to proceed", "OK");
+                return;
+            }
+            cheeseBrand = brand.Text;
+            cheeseType = type.Text;
+            cheeseComments = comments.Text;
+            cheeseLocation = location.Text;
+            cheesePrice = Double.Parse(price.Text);
+
 
         }
 
